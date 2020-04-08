a4701da7401a8947e1ad0e197a84bcbb3ec50129
Janis Karlsson <janis_k@hotmail.com>
2020-04-08 11:50:16 +0200

    database export to individual files
    Peter + Janis

---
+++ backend/database/hoolitels/amenity.sql
@@ -0,0 +1,26 @@
+-- --------------------------------------------------------
+-- Värd:                         127.0.0.1
+-- Serverversion:                10.4.11-MariaDB - mariadb.org binary distribution
+-- Server-OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumpar struktur för tabell hoolitels.amenity
+CREATE TABLE IF NOT EXISTS `amenity` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- Dataexport var bortvalt.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ backend/database/hoolitels/hotelamenity.sql
@@ -0,0 +1,30 @@
+-- --------------------------------------------------------
+-- Värd:                         127.0.0.1
+-- Serverversion:                10.4.11-MariaDB - mariadb.org binary distribution
+-- Server-OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumpar struktur för tabell hoolitels.hotelamenity
+CREATE TABLE IF NOT EXISTS `hotelamenity` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `hotelId` int(11) NOT NULL,
+  `amenityId` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `hotelId_amenityId` (`hotelId`,`amenityId`),
+  KEY `FK_hotelamenity_amenity` (`amenityId`),
+  CONSTRAINT `FK_hotelamenity_amenity` FOREIGN KEY (`amenityId`) REFERENCES `amenity` (`Id`),
+  CONSTRAINT `FK_hotelamenity_hotel` FOREIGN KEY (`hotelId`) REFERENCES `hotel` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dataexport var bortvalt.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
