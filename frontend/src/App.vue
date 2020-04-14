1c9c5cd4d535d821d5f7206f6ee8e20dc4f7fa11
razvan22 <razviy69@gmail.com>
2020-04-14 09:58:22 +0200

    addLogo
    Peter&Sofia

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <h1>Hoolitels</h1>
-    <a @click="getUsers" class="waves-effect waves-light btn">button</a>
-    <h3 v-for="user in users" :key="user.id" >{{user.name}}</h3>
-    <router-link to="/about">About</router-link>
-   
+    
   </div>
 </template>
 
