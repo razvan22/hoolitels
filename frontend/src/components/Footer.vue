1ffb1b1ed57cc48fd3e6954c24e1f2c14defa677
razvan22 <razviy69@gmail.com>
2020-04-14 13:57:12 +0200

    Revert "Merge branch 'header-navbar' of https://github.com/razvan22/hoolitels into header-navbar"
    This reverts commit a38aa41e520418f897131841c180983310ee5dc4, reversing
    changes made to a84fc8fef597c78cd263c461e988f035ec464a4b.

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,47 +1,17 @@
+
+
 <template>
-<ul class ="Footer">
-    <span class= "fab fa-facebook fa-3x "></span>
-    <span class="fab fa-instagram fa-3x "></span>
-    <span class="fab fa-twitter-square fa-3x"></span>
-      <div><span id="footer-text"> @Copyright Hoolitels {{currentYear}} </span> </div>
-  </ul>
-
+  <div><h1>footer</h1></div>
 </template>
-
-
 
 <script>
 export default {
-    name:'Footer',
-    data(){
-      return{
-        currentYear: new Date().getFullYear(),
-      }
-    }
+    name:'Footer'
 }
 </script>
 
 <style>
-    ul{
-       box-sizing: border-box;
-         border: 1px solid black;
-         bottom: 0;
-         position: absolute;
-        width: 100%;
-       
+    body{
+        background-color: aquamarine;
     }
-    span  {
-      padding: 5%;
-    }
-    .fa-facebook {
-      color: blue;
-    }
-     .fa-instagram{
-      color:purple;
-    }
-    
-     .fa-twitter-square{
-      color:lightblue;
-    }
-  
 </style>
