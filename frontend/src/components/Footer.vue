1977b925d07788a4c7fa7a4febfc5b9bd3338f32
Sofia <sofia.fredmans@gmail.com>
2020-04-14 12:45:56 +0200

    footer done, pls help with fontawesome icons

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,17 +1,41 @@
-
-
 <template>
-  <div><h1>footer</h1></div>
+<ul class ="Footer">
+    <span v-for="icon in icons" :key="icon.image"> {{icon.image}}</span>
+      <div><span id="footer-text"> @Copyright Hoolitels {{currentYear}} </span> </div>
+  </ul>
+
 </template>
+
+
 
 <script>
 export default {
-    name:'Footer'
+    name:'Footer',
+    data(){
+      return{
+        currentYear: new Date().getFullYear(),
+        icons: [
+          {image: 'Facebook'}, 
+          {image:'Instagram'}, 
+          {image:'Twitter'}, 
+
+        ]
+      }
+    }
 }
 </script>
 
 <style>
-    body{
-        background-color: aquamarine;
+    ul{
+       box-sizing: border-box;
+         border: 1px solid black;
+         bottom: 0;
+         position: absolute;
+        width: 100%;
+       
     }
+    span  {
+      padding: 5%;
+    }
+  
 </style>
