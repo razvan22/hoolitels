e3eff49ed66f88560aaf9f8e52f25a503ce0c1f5
razvan22 <razviy69@gmail.com>
2020-04-15 17:22:52 +0200

    login_v1.2
    without functions !

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -15,6 +15,13 @@
             <label for="password" data-error="wrong" data-success="right">Password</label>
           </div>
         </div>
+        <div class="col s12">
+            <button class="btn waves-effect waves-light  blue darken-3" type="submit" name="action">LOGIN
+            </button>
+        </div>
+      </div>
+      <div class="row row-signup">
+         <a href="">Don't have an account ? Sign up here</a>
       </div>
     </div>
   </div>
@@ -38,6 +45,10 @@ export default {
     }
   .container .row .col .input-field input[type=password]:focus + label {
      color: #000 !important;
-     }
+    }
+
+  .container .row-signup{
+    margin-top: 7em;
+  }
 
 </style>
