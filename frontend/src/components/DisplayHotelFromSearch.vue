491478ac9f5e78df0b6d036edf31d61aad6262c8
Janis Karlsson <janis_k@hotmail.com>
2020-04-19 01:57:08 +0200

    EOD
    End-Of-Day commit
    Started on the room component
    Adjusted the hotel component

--- frontend/src/components/DisplayHotelFromSearch.vue
+++ frontend/src/components/DisplayHotelFromSearch.vue
@@ -1,73 +1,58 @@
 <template>
   <div class="container hotel-from-search">
-        <div class="row">
-    <div class="col s12 m12 l12">
-      <div class="card">
-        <div class="card-image">
-          <img src="https://static.thatsup.co/content/img/article/15/jul/guiden-till-prisvarda-hotell-i-stockholm.jpg">
-          <span class="card-title">{{hotel.name}}</span>
-        </div>
-        <div class="card-content " >
-           <p>
-              <ul>
-                <li v-for="a in hotel.amenities" :key="a.id">{{a.name}}</li>
-            </ul>
-           </p>
-        </div>
-        <div class="card-action">
-          <a href="#">link to hotel vy</a>
+    <div class="row">
+      <div class="col s12 m12 l12">
+        <div class="card hoverable">
+          <div class="card-image">
+            <img
+              src="https://static.thatsup.co/content/img/article/15/jul/guiden-till-prisvarda-hotell-i-stockholm.jpg"
+            />
+            <span class="card-title">{{ hotel.name }}</span>
+          </div>
+          <div class="card-content">
+            <div class="row">
+              <ul class="amenities">
+                <li v-for="a in hotel.amenities" :key="a.id">
+                  <div class="valign-wrapper">
+                    <i class="material-icons teal-text">check_circle</i
+                    >{{ a.name }}
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="row">
+              <div class="col s12">
+                <RoomView v-for="r in hotel.rooms" :key="r.id" :room="r" />
+              </div>
+            </div>
+          </div>
+          <div class="card-action">
+            <a href="#">link to hotel vy</a>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
-    <!-- <div class="card" >
-      <div class="card-image">
-        <img src="https://static.thatsup.co/content/img/article/15/jul/guiden-till-prisvarda-hotell-i-stockholm.jpg">
-        <span class="card-title">{{hotel.name}}</span>
-      </div>
-      <div class="card-content">
-          <ul class="collection">
-            <li class="collection-item" v-for="a in hotel.amenities" :key="a.id">{{a.name}}</li>
-          </ul>
-      </div>
-      <div class="card-action">
-           <a href="#">This is a link</a>
-      </div>
-    </div> -->
-       
-  </div>
 </template>
 
 <script>
+import RoomView from "@/components/RoomView.vue";
 
 export default {
-    name: 'DisplayHotelFromSearch',
-    props: [`hotel`],
-    data(){
-      return{
-        amenities:[
-          {
-          name:'this',
-          id:0
-        },
-          {
-          name:'second',
-          id:2
-        }
-        ]
-      }
-    }
-    ,
-    mounted(){
-      //  let h = document.querySelectorAll('.hotel-from-search');
-    
-        
-    }
-
-}
+  name: "DisplayHotelFromSearch",
+  props: [`hotel`],
+  data() {
+    return {};
+  },
+  mounted() {},
+  components: {
+    RoomView,
+  },
+};
 </script>
 
 <style>
-
+.amenities {
+  font-size: smaller;
+}
 </style>
