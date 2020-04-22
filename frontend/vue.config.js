b9a0a2ce4de3b11ba6d1b2a2f132eb9c59c7b60f
Mathias <mathias.permlid@gmail.com>
2020-04-22 10:54:15 +0200

    Login
    Login works - tnx Dennis

---
+++ frontend/vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '^/rest': {
+        target: 'http://localhost:8070',
+        ws: true,
+        changeOrigin: true,
+      },
+      '^/login': {
+        target: 'http://localhost:8070',
+        ws: true,
+        changeOrigin: true,
+      },
+      '^/api': {
+        target: 'http://localhost:8070',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+}
