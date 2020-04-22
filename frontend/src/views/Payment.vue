705f3d08feb04c9b95aaf437a1fa08755ad5292f
Sofia <sofia.fredmans@gmail.com>
2020-04-22 10:03:01 +0200

    Styling done

--- frontend/src/views/Payment.vue
+++ frontend/src/views/Payment.vue
@@ -1,46 +1,44 @@
 <template>
-  <div class="container">
-      <Header/>
-       <div class="payment-form row">
-          <div class="col s12">
-                 <div class="input-field col s12">
+    <div class="body">
+        <Header/>
+            <div class="container payment-form">
+                <div class="row">
+                    <div class="input-field col s12">
                     <input placeholder="XXXX-XXXX-XXXX-XXXX"  type="text" class="validate">
                     <label for="first_name">Kortnummer</label>
                  </div>
-          </div>
-          <div class="row">
-              <div class="col s6">
-                    <div class="input-field col s12">
-                    <input placeholder="XXX/XXX"  type="text" class="validate">
+                </div>
+                <div class="row">
+                    <div class="input-field col s6">
+                     <input placeholder="XXX/XXX"  type="text" class="validate">
                     <label for="first_name">CVC</label>
-                 </div>
-              </div>
-                   <div class="col s6">
-                        <div class="input-field col s12">
-                            <input placeholder="XX/XX"  type="text" class="validate">
-                            <label for="first_name">Utgångsdatum</label>
-                        </div>
-                    </div>
-                    <div class="col s12 center-align">
-                              <router-link
+                </div>
+              <div class="input-field col s6">
+                    <input placeholder="XX/XX"  type="text" class="validate">
+                    <label for="first_name">Utgångsdatum</label>
+                </div>
+                <div class="row">
+                    <div class="col align-center offset-s4 offset-m5 offset-l5">
+                         <router-link
                                 to="/"
                                 a class="waves-effect waves-light btn cyan darken-2"> Betala
                             </router-link>
-                       
+
                     </div>
                 </div>
-        
        </div>
-
-      <Footer/>
-     
-  </div>
-
+    </div>
+    <Footer/>
+    </div>
 </template>
 
 <script>
-import Footer from '../components/Footer.vue'
+
+
+
 import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+
 
 export default {
     data(){
@@ -48,7 +46,9 @@ export default {
     },
     components:{
         Header,
-        Footer,
+        Footer
+     
+     
     },
 }
 </script>
@@ -57,6 +57,9 @@ export default {
 <style>
 .payment-form{
     background: white;
+      padding-top: 2vh;
+    
+    
 }
 
 </style>
