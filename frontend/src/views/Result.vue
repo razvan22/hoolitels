fed5a32835770dca2b8b4c1bf1ca6ea9c825d8eb
Janis <janis_k@hotmail.com>
2020-04-28 19:13:57 +0200

    Sort and filter
    Sort and filter halfway done; changing driver
    Sofia, Mathias, Janis

--- frontend/src/views/Result.vue
+++ frontend/src/views/Result.vue
@@ -1,16 +1,26 @@
 <template>
-<body>
-      <SortAndFilter/>
-      <DisplayHotel v-for="h in city.hotels" :key="h.id" :hotel="h" :DisplayRooms="false"/>
-</body>
+  <body>
+    <div>
+      <h1>Sökparametrar</h1>
+      <span>{{ this.$store.state.resultSortFilter.selected }}</span>
+      <span>,{{ this.$store.state.resultSortFilter.checkedFiltration }}</span>
+      <span>,{{ this.$store.state.resultSortFilter.dist_to_beach }}</span>
+      <span>,{{ this.$store.state.resultSortFilter.dist_to_town }}</span>
+    </div>
+    <SortAndFilter />
+    <DisplayHotel
+      v-for="h in city.hotels"
+      :key="h.id"
+      :hotel="h"
+      :DisplayRooms="false"
+    />
+  </body>
 </template>
 
 <script>
-
-
 import DisplayHotel from "@/components/DisplayHotel.vue";
 import SortAndFilter from "@/components/SortAndFilter.vue";
-
+import { bus } from "../main";
 
 export default {
   data() {
@@ -19,19 +29,26 @@ export default {
     };
   },
 
+  created() {
+    bus.$on("changeIt", (data) => {
+      console.log("We got some new data from the filter!!!", data);
+      this.getCity();
+    });
+  },
+
   mounted() {
     this.getCity();
   },
 
   components: {
     DisplayHotel,
-    SortAndFilter 
+    SortAndFilter,
   },
+
   methods: {
     getCity: async function() {
       let response = await fetch(
-        "/rest/city/" +
-          this.$store.state.dateSelected.selectedCity
+        "/rest/city/" + this.$store.state.dateSelected.selectedCity
       );
       response = await response.json();
       this.city = response;
