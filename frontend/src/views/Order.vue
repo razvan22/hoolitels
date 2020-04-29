c6d3e692d6bca15ec4e0434106673e709d900ba8
Janis <janis_k@hotmail.com>
2020-04-29 23:03:38 +0200

    Booking done!
    Every one participated more or less all the time except the last hour

--- frontend/src/views/Order.vue
+++ frontend/src/views/Order.vue
@@ -1,121 +1,182 @@
 <template>
-<div class="container">
-   <div class="row">
-    <div class="col s12 offset-l1 l9 center-align">
-      <div class="card-background">
-       <div >
-        <RoomView
-            v-for="r in rooms"
-            :key="r.id"
-            :room="r"
-          />
-        </div>      
-        <h6>Vem kommer med?</h6>
-         <h5>Gäster {{counterAdult + counterChild +counterBaby}}</h5>
-         <div class="guests center-align">
-           <p><strong> Vuxna </strong></p>
-           <a class="btn-floating cyan darken-2" v-on:click="counterDecrementAdult()" >
-             <i class="material-icons">remove</i></a> {{counterAdult}} <a class="btn-floating cyan darken-2"
-              v-on:click="counterAdult+=1"><i class="material-icons">add</i></a>
-         </div>
-          <div class="guests center-align ">
-           <p><strong> Barn </strong></p>
-           <a class="btn-floating cyan darken-2" v-on:click="counterDecrementChild()">
-             <i class="material-icons">remove</i></a> {{counterChild}} <a class="btn-floating cyan darken-2"
-              v-on:click="counterChild+=1"><i class="material-icons">add</i></a>
-         </div>
-          <div class="last-button guests center-align ">
-           <p><strong> Spädbarn </strong></p>
-           <a class="btn-floating cyan darken-2"  v-on:click="counterDecrementBaby()">
-             <i class="material-icons">remove</i></a> {{counterBaby}} <a class="btn-floating cyan darken-2"
-              v-on:click="counterBaby+=1"><i class="material-icons">add</i></a>
-         </div>
-         <div class="divider"></div>   
-      <div class="row">
-        <form class="col s12 ">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">mode_edit</i>
-            <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-            <label for="icon_prefix2">Meddelande till hotellet</label>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 offset-l1 l9 center-align">
+        <div class="card-background">
+          <div>
+            <RoomView v-for="r in rooms" :key="r.id" :room="r" />
           </div>
-           </form>
-        <div class="col s12 center-align">
-          <router-link to="/payment">  <a class="btn-continue-payment waves-effect waves-light btn-small cyan darken-2 r">Fortsätt till betalning</a></router-link>
-        </div> 
-      </div> 
+          <h5>Vem kommer med?</h5>
+          <h6>Antal gäster {{ counterAdult + counterChild + counterBaby }}</h6>
+          <div class="guests center-align">
+            <p><strong> Vuxna </strong></p>
+            <a
+              class="btn-floating cyan darken-2"
+              v-on:click="counterDecrementAdult()"
+            >
+              <i class="material-icons">remove</i></a
+            >
+            {{ counterAdult }}
+            <a class="btn-floating cyan darken-2" v-on:click="counterAdult += 1"
+              ><i class="material-icons">add</i></a
+            >
+          </div>
+          <div class="guests center-align ">
+            <p><strong> Barn </strong></p>
+            <a
+              class="btn-floating cyan darken-2"
+              v-on:click="counterDecrementChild()"
+            >
+              <i class="material-icons">remove</i></a
+            >
+            {{ counterChild }}
+            <a class="btn-floating cyan darken-2" v-on:click="counterChild += 1"
+              ><i class="material-icons">add</i></a
+            >
+          </div>
+          <div class="last-button guests center-align ">
+            <p><strong> Spädbarn </strong></p>
+            <a
+              class="btn-floating cyan darken-2"
+              v-on:click="counterDecrementBaby()"
+            >
+              <i class="material-icons">remove</i></a
+            >
+            {{ counterBaby }}
+            <a class="btn-floating cyan darken-2" v-on:click="counterBaby += 1"
+              ><i class="material-icons">add</i></a
+            >
+          </div>
+          <div class="divider"></div>
+          <div class="row">
+            <form class="col s12 ">
+              <div class="input-field col s12">
+                <i class="material-icons prefix">mode_edit</i>
+                <textarea
+                  v-model="message"
+                  id="icon_prefix2"
+                  class="materialize-textarea"
+                ></textarea>
+                <label for="icon_prefix2">Meddelande till hotellet</label>
+              </div>
+            </form>
+            <div class="col s12 center-align" v-if="loggedIn">
+              <router-link to="/payment">
+                <a
+                  class="btn-continue-payment waves-effect waves-light btn-small cyan darken-2 r"
+                  >Fortsätt till betalning</a
+                ></router-link
+              >
+            </div>
+            <div class="col s12 center-align" v-else>
+              <div
+                class="waves-effect waves-light btn cyan darken-2 sidenav-trigger btn-continue-payment"
+                data-target="slide-out"
+              >
+                För att fortsätta måste du först logga in!
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
-
 import RoomView from "@/components/RoomView";
 
 export default {
-
   components: {
     RoomView,
   },
 
-  data(){
-    return{
-      counterAdult: 0,
-      counterChild: 0,
-      counterBaby: 0,
-    }
+  data() {
+    return {
+      // counterAdult: 0,
+      // counterChild: 0,
+      // counterBaby: 0,
+    };
   },
 
   computed: {
+    loggedIn() {
+      return this.$store.state.userLogged;
+    },
+
     rooms() {
       return this.$store.state.roomSelection.roomsSelected;
-      // let rArray = [];
-      // for (let i = 0; i < this.$store.state.roomSelection.roomIdPerType.length; i++) {
-      //   if (this.$store.state.roomSelection.roomIdPerType[i])
-      //     rArray = [...rArray, ...this.$store.state.roomSelection.roomIdPerType[i]];
-      // }
+    },
 
-      // return rArray;
+    message: {
+      get: function() {
+        return this.$store.state.bookingInfo.message;
+      },
+
+      set: function(value) {
+        this.$store.commit("setMessageToHotel", value);
+      },
+    },
+
+    counterAdult: {
+      get: function() {
+        return this.$store.state.bookingInfo.numAdults;
+      },
+
+      set: function(value) {
+        this.$store.commit("setNumAdults", value);
+      },
+    },
+
+    counterChild: {
+      get: function() {
+        return this.$store.state.bookingInfo.numChildren;
+      },
+
+      set: function(value) {
+        this.$store.commit("setNumChildren", value);
+      },
+    },
+
+    counterBaby: {
+      get: function() {
+        return this.$store.state.bookingInfo.numToddlers;
+      },
+
+      set: function(value) {
+        this.$store.commit("setNumToddlers", value);
+      },
     },
   },
 
-  methods:{
-    counterDecrementAdult(){
-      this.counterAdult -=1;
-      if (this.counterAdult < 0 )
-      this.counterAdult = 0;
-      
+  methods: {
+    counterDecrementAdult() {
+      this.counterAdult -= 1;
+      if (this.counterAdult < 0) this.counterAdult = 0;
     },
-       counterDecrementChild(){
-      this.counterChild -=1;
-      if (this.counterChild < 0 )
-      this.counterChild = 0;
-      
+    counterDecrementChild() {
+      this.counterChild -= 1;
+      if (this.counterChild < 0) this.counterChild = 0;
     },
-        counterDecrementBaby(){
-      this.counterBaby -=1;
-      if (this.counterBaby < 0 )
-      this.counterBaby = 0;
-      
+    counterDecrementBaby() {
+      this.counterBaby -= 1;
+      if (this.counterBaby < 0) this.counterBaby = 0;
     },
-  }
-}
+  },
+};
 </script>
 
 <style>
-.card-background{
-  background:white;
+.card-background {
+  background: white;
   padding-bottom: 1%;
-
 }
 
-.btn-continue-payment{
+.btn-continue-payment {
   border-radius: 25px;
-
 }
 
-.last-button{
+.last-button {
   margin-bottom: 7%;
 }
 </style>
--- frontend/src/views/Payment.vue
+++ frontend/src/views/Payment.vue
@@ -1,65 +1,243 @@
 <template>
   <div class="body">
-      <div id="modal1" class="modal">
-    <div class="modal-content">
-      <div class="center-align">
-         <i class="green-text material-icons medium">check_circle</i>
-      </div>
-      <h5 class="center-align">Vi har tagit emot din betalning</h5>
-      <p class="center-align">Tack för din bokning</p>
-      <div class="divider"></div>
-    </div>
-    <div class="modal-footer center-align">
-      <a href="/userProfile" class="modal-close waves-effect waves-light ">Klicka här för att se dina bokningar</a>
-    </div>
-  </div>
     <div class="container payment-form">
       <div class="row">
         <div class="input-field col s12">
-          <input placeholder="Förnamn och efternamn" type="text" class="validate" id="first_name" required ="" aria-required="true" />
+          <input
+            placeholder="Förnamn och efternamn"
+            type="text"
+            class="validate"
+            id="first_name"
+            required=""
+            aria-required="true"
+          />
           <label for="first_name" class="active">Kortinnehavare</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input placeholder="XXXX-XXXX-XXXX-XXXX" type="number" class="validate" id="card_number" required="" pattern="[0-4]" aria-required="true"  />
+          <input
+            placeholder="XXXX-XXXX-XXXX-XXXX"
+            type="number"
+            class="validate"
+            id="card_number"
+            required=""
+            pattern="[0-4]"
+            aria-required="true"
+          />
           <label for="card_number" class="active">Kortnummer</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          <input placeholder="XXX" type="number" class="validate" id="cvc" required="" aria-required="true" />
+          <input
+            placeholder="XXX"
+            type="number"
+            class="validate"
+            id="cvc"
+            required=""
+            aria-required="true"
+          />
           <label for="cvc" class="active">CVC</label>
         </div>
         <div class="input-field col s6">
-          <input placeholder="XX/XX" type="number" class="validate" id="expiration_date" required="" aria-required="true" />
+          <input
+            placeholder="XX/XX"
+            type="number"
+            class="validate"
+            id="expiration_date"
+            required=""
+            aria-required="true"
+          />
           <label for="expiration_date" class="active">Utgångsdatum</label>
         </div>
         <div class="row">
-          <div class="col align-center offset-s4 offset-m5 offset-l5">
-             <a class="btn-payment waves-effect waves-light btn modal-trigger cyan darken-2" href="#modal1" @click="saveBooking">Betala</a>
+          <!-- <div class="col align-center offset-s4 offset-m5 offset-l5"> -->
+          <div class="col align-center offset-s1 offset-m4 offset-l4">
+            <a
+              class="btn-payment waves-effect waves-light btn modal-trigger cyan darken-2 align-center"
+              @click="saveBooking"
+              >Godkänn och betala din bokning</a
+            >
           </div>
         </div>
       </div>
     </div>
-  </div> 
+    <div>
+      <modal
+        name="bookingsuccessful"
+        :height="230"
+        :width="500"
+        :border-radius="200"
+      >
+        <div class="promp">
+          <div class="modal-content">
+            <div class="center-align">
+              <i class="green-text material-icons medium">check_circle</i>
+            </div>
+            <h5 class="center-align">Vi har tagit emot din betalning</h5>
+            <p class="center-align">Tack för din bokning!</p>
+          </div>
+          <div class="modal-footer center-align">
+            <router-link to="/userprofile">
+              <div
+                class="waves-effect waves-light btn cyan darken-2 btn-payment"
+              >
+                Klicka här för att gå till din profil
+              </div>
+            </router-link>
+          </div>
+        </div>
+      </modal>
+    </div>
+    <modal name="bookingfailed" :height="150" :width="500" :border-radius="200">
+      <div id="promp">
+        <h5 class="center red-text">
+          Fel vid bokning! Var god försök senare.
+        </h5>
+
+        <div class="center">
+          <button
+            @click="hide"
+            class="btn-register-2 btn-small waves-effect waves-light cyan darken-2"
+            name="action"
+          >
+            Ok
+          </button>
+        </div>
+      </div>
+    </modal>
+  </div>
 </template>
 
 <script>
-import M from 'materialize-css'
+// import M from "materialize-css";
 
-export default{
-  mounted(){
-    let elems = document.querySelectorAll('.modal');
-    M.Modal.init(elems);
+export default {
+  mounted() {
+    // let elems = document.querySelectorAll(".modal");
+    // M.Modal.init(elems);
+  },
+
+  computed: {
+    rooms: {
+      get: function() {
+        return this.$store.state.roomSelection.roomsSelected;
+      },
+    },
+
+    numAdults: {
+      get: function() {
+        return this.$store.state.bookingInfo.numAdults;
+      },
+
+      set: function(value) {
+        this.$store.commit("setAdults", value);
+      },
+    },
+
+    numChildren: {
+      get: function() {
+        return this.$store.state.bookingInfo.numChildren;
+      },
+
+      set: function(value) {
+        this.$store.commit("setChildren", value);
+      },
+    },
+
+    numToddlers: {
+      get: function() {
+        return this.$store.state.bookingInfo.numToddlers;
+      },
+
+      set: function(value) {
+        this.$store.commit("setToddlers", value);
+      },
+    },
+
+    startDate() {
+      return this.$store.state.booking.check_in;
+    },
+
+    endDate() {
+      return this.$store.state.booking.check_out;
+    },
+
+    message: {
+      get: function() {
+        return this.$store.state.bookingInfo.message;
+      },
+
+      set: function(value) {
+        this.$store.commit("setMessageToHotel", value);
+      },
+    },
   },
 
   methods: {
-    saveBooking: function() {
+    saveBooking: async function() {
+      let bookingObj = {};
 
-    }
-  }
+      bookingObj.user = { id: this.$store.state.user.id };
+      bookingObj.roombookings = [];
+      this.rooms.forEach((r) => {
+        bookingObj.roombookings.push({
+          room: { id: r.id },
+          extra_bed: 0,
+          food_cost: 0,
+          room_cost: r.price,
+        });
+      });
 
+      bookingObj.num_adults = this.numAdults;
+      bookingObj.num_children = this.numChildren;
+      bookingObj.num_infants = this.numToddlers;
+      bookingObj.start_date = this.startDate;
+      bookingObj.end_date = this.endDate;
+      bookingObj.comment = this.message;
+      bookingObj.paid = true;
+
+      console.log("bookingObj: ", JSON.stringify(bookingObj));
+
+      let rawResponse = await fetch("/rest/booking", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept-Encoding": "gzip, deflate, br",
+        },
+        body: JSON.stringify(bookingObj),
+      });
+
+      console.log(rawResponse);
+
+      if (rawResponse.status == 200) {
+        this.numAdults = 0;
+        this.numChildren = 0;
+        this.numToddlers = 0;
+        this.message = "";
+        this.$store.commit("clearSelectedRooms");
+        this.successfulShow();
+      } else {
+        this.failedShow();
+      } // else
+
+      let response = await rawResponse.json();
+      console.log(response);
+    },
+
+    successfulShow() {
+      this.$modal.show("bookingsuccessful");
+    },
+
+    failedShow() {
+      this.$modal.show("bookingfailed");
+    },
+
+    hide() {
+      this.$modal.hide("bookingfailed");
+    },
+  },
 };
 </script>
 
@@ -68,16 +246,15 @@ export default{
   background: white;
   padding-top: 2vh;
 }
-.btn-payment{
-   border-radius: 25px;
+.btn-payment {
+  border-radius: 25px;
 }
 
-.modal-footer{
+.modal-footer {
   text-align: center;
 }
 
-.modal-content{
+.modal-content {
   padding-bottom: 0.2em;
 }
-
 </style>
