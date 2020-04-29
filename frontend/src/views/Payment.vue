f81282b660eff3d712a68b6ff7c9972e2841a1fc
Sofia <sofia.fredmans@gmail.com>
2020-04-29 11:40:36 +0200

    modal done. takes you to profile page

--- frontend/src/views/Payment.vue
+++ frontend/src/views/Payment.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="body">
-     
-
       <div id="modal1" class="modal">
     <div class="modal-content">
+      <div class="center-align">
+         <i class="green-text material-icons medium">check_circle</i>
+      </div>
       <h5 class="center-align">Vi har tagit emot din betalning</h5>
-      <p class="center-align">Tack för din bokning </p>
+      <p class="center-align">Tack för din bokning</p>
+      <div class="divider"></div>
+       
     </div>
     <div class="modal-footer center-align">
-      <a href="/" class="modal-close waves-effect waves-green btn-flat">Ta mig till startsidan</a>
+      <a href="/userProfile" class="modal-close waves-effect waves-light ">Klicka här för att se dina bokningar</a>
     </div>
   </div>
-    
-
-
-
     <div class="container payment-form">
       <div class="row">
         <div class="input-field col s12">
-          <input placeholder="Förnamn och efternamn" type="text" class="validate" id="first_name" required="" aria-required="true" />
+          <input placeholder="Förnamn och efternamn" type="text" class="validate" id="first_name" required ="" aria-required="true" />
           <label for="first_name" class="active">Kortinnehavare</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input placeholder="XXXX-XXXX-XXXX-XXXX" type="number" class="validate" id="card_number" required="" aria-required="true" />
+          <input placeholder="XXXX-XXXX-XXXX-XXXX" type="number" class="validate" id="card_number" required="" pattern="[0-4]" aria-required="true"  />
           <label for="card_number" class="active">Kortnummer</label>
         </div>
       </div>
@@ -39,14 +38,12 @@
         </div>
         <div class="row">
           <div class="col align-center offset-s4 offset-m5 offset-l5">
-             <a class="btn-payment waves-effect waves-light btn modal-trigger  cyan darken-2" href="#modal1">Betala </a>
+             <a class="btn-payment waves-effect waves-light btn modal-trigger cyan darken-2" href="#modal1">Betala </a>
           </div>
         </div>
       </div>
     </div>
-  </div>
-
-  
+  </div> 
 </template>
 
 <script>
@@ -73,4 +70,13 @@ export default{
 .btn-payment{
    border-radius: 25px;
 }
+
+.modal-footer{
+  text-align: center;
+}
+
+.modal-content{
+  padding-bottom: 0.2em;
+}
+
 </style>
