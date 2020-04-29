3c5b4c07a428a557affb8a23e31b60b7de3d6a3f
Mathias <mathias.permlid@gmail.com>
2020-04-29 16:39:49 +0200

    Minor css fixes
    fixes for readability

--- frontend/src/components/DisplayHotel.vue
+++ frontend/src/components/DisplayHotel.vue
@@ -5,13 +5,21 @@
       <div class="card hoverable">
         <div class="card-image">
           <img :src="require('@/assets/' + hotel.images[0].url)" />
-          <span class="card-title"
-            ><strong class="hotel-name">{{ hotel.name }}</strong></span
-          >
+          <span class="card-title">{{ hotel.name }}</span>
         </div>
         <div class="card-content">
           <div class="row">
-            <div class="col s5 m4 l3">
+            <div class="contact col s5 m4 l3">
+              <h4>Omdöme</h4>
+              <div>
+                <i
+                  v-for="n in 4"
+                  :key="n"
+                  class=" rating__icon--star fa fa-star"
+                ></i>
+              </div>
+            </div>
+            <div class="col s5 m4 l3 ">
               <ul class="amenities">
                 <h4 v-if="amenitiesIsEmpty">Bekvämligheter</h4>
                 <li v-for="a in hotel.amenities" :key="a.id">
@@ -21,16 +29,6 @@
                   </div>
                 </li>
               </ul>
-            </div>
-            <div class="contact col offset-s2 s5 m4 l3">
-              <h4>Omdöme</h4>
-              <div>
-                <i
-                  v-for="n in 4"
-                  :key="n"
-                  class=" rating__icon--star fa fa-star"
-                ></i>
-              </div>
             </div>
           </div>
           <div class="row">
@@ -88,102 +86,103 @@
   </div>
 </template>
 <script>
-// import RoomView from "@/components/RoomView.vue";
-import RoomTypeList from "@/components/RoomTypeList";
-import { bus } from "../main";
+  // import RoomView from "@/components/RoomView.vue";
+  import RoomTypeList from '@/components/RoomTypeList'
+  import { bus } from '../main'
 
-export default {
-  name: "DisplayHotel",
-  props: [`hotel`, `DisplayRooms`],
-  data() {
-    return {
-      isHidden: false,
-      selectedRoomsPerType: [0, 0, 0, 0],
-      totRoomsSelected: 0,
-    };
-  },
-  computed: {
-    buttonShouldBeDisabled: {
-      get() {
-        return this.totRoomsSelected < this.$store.state.booking.rooms;
+  export default {
+    name: 'DisplayHotel',
+    props: [`hotel`, `DisplayRooms`],
+    data() {
+      return {
+        isHidden: false,
+        selectedRoomsPerType: [0, 0, 0, 0],
+        totRoomsSelected: 0,
       }
     },
+    computed: {
+      buttonShouldBeDisabled: {
+        get() {
+          return this.totRoomsSelected < this.$store.state.booking.rooms
+        },
+      },
 
-    amenitiesIsEmpty() {
-      return this.hotel.amenities && this.hotel.amenities.length;
-    },
-  },
-
-  created() {
-    bus.$on("selectedRooms", (data) => {
-      this.selectedRoomsPerType[data.listNum] = parseInt(data.newVal);
-      this.calcTotRoomsSelected();
-    });
-  },
-
-  components: {
-    RoomTypeList,
-  },
-
-  methods: {
-    calcTotRoomsSelected: function() {
-      let sum = 0;
-      for (let i = 0; i < this.selectedRoomsPerType.length; i++) {
-        sum += this.selectedRoomsPerType[i];
-      }
-      this.totRoomsSelected = sum;
-      // return sum;
+      amenitiesIsEmpty() {
+        return this.hotel.amenities && this.hotel.amenities.length
+      },
     },
 
-    getSum: function(total, num) {
-      return total + parseInt(num);
+    created() {
+      bus.$on('selectedRooms', (data) => {
+        this.selectedRoomsPerType[data.listNum] = parseInt(data.newVal)
+        this.calcTotRoomsSelected()
+      })
     },
-  },
-};
+
+    components: {
+      RoomTypeList,
+    },
+
+    methods: {
+      calcTotRoomsSelected: function() {
+        let sum = 0
+        for (let i = 0; i < this.selectedRoomsPerType.length; i++) {
+          sum += this.selectedRoomsPerType[i]
+        }
+        this.totRoomsSelected = sum
+        // return sum;
+      },
+
+      getSum: function(total, num) {
+        return total + parseInt(num)
+      },
+    },
+  }
 </script>
 
 <style scoped>
-/* ändra h4 till en klass */
-h4 {
-  margin-top: 10px;
-}
+  /* ändra h4 till en klass */
+  h4 {
+    margin-top: 10px;
+  }
 
-.row {
-  margin-bottom: 0px;
-}
-.rating__icon--star {
-  color: orange;
-  font-size: 18px;
-}
+  .row {
+    margin-bottom: 0px;
+  }
+  .rating__icon--star {
+    color: orange;
+    font-size: 18px;
+  }
 
-.amenities {
-  font-size: smaller;
-}
+  .amenities {
+    font-size: smaller;
+  }
 
-.amenities h4 {
-  font-size: large;
-}
+  .amenities h4 {
+    font-size: large;
+  }
 
-.distance {
-  font-size: smaller;
-}
-.distance h4 {
-  font-size: large;
-}
+  .distance {
+    font-size: smaller;
+  }
+  .distance h4 {
+    font-size: large;
+  }
 
-.contact {
-  font-size: smaller;
-}
+  .contact {
+    font-size: smaller;
+  }
 
-.contact h4 {
-  font-size: large;
-}
-.hotel-name {
-  text-shadow: 5px;
-  color: black;
-  background: white;
-}
-.card-image img {
-  opacity: 0.8;
-}
+  .contact h4 {
+    font-size: large;
+  }
+  .card-title {
+    text-shadow: 1px 1px black !important;
+    background: rgba(253, 253, 253, 0.3);
+    width: 100%;
+    padding: 10px 15px !important;
+  }
+  .card-image img {
+    /* opacity: 0.8; */
+  }
 </style>
