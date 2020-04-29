78b5ce813ee0d9d8b505d0384042750265c6405e
Steve Chapman <srchap78@gmail.com>
2020-04-29 15:22:15 +0200

    work in progress

--- frontend/tests/selenium/step-definitions/roomsByCity.js
+++ frontend/tests/selenium/step-definitions/roomsByCity.js
@@ -9,13 +9,12 @@ module.exports = function () {
     });
 
     this.When(/^I select Malmö from the välj stad dropdown menu$/, async function () {
-        let dropDown = await $('body > main > div > div.row > div:nth-child(1)');
+        let dropDown = await driver.findElements(by.innerHTML('Välj stad'));
         dropDown.click();
         await sleep(200);
-        //let malmo = $('#select-options-db935abb-f02c-41e7-6c9a-303f8d1d60dd7');
-        //let malmo = await driver.findElements(by.text('Malmö'));
-        var xpath = "//li[contains(text(),'Malmö')]";
-        xpath.click();
+        var malmo = await driver.findElement(by.innerHTML('Malmö'));
+        console.log(malmo);
+        malmo.click();
         await sleep(200)
         //write assert test that Malmö is in the välj stad dropdown
     });
