01f8f473ded276bc4af11bb20f7e9aa7c1a43c22
Steve Chapman <srchap78@gmail.com>
2020-05-01 13:27:53 +0200

    setup and and began unit testing.

---
+++ frontend/tests/unit/searchByCity.spec.js
@@ -0,0 +1,17 @@
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks()
+
+import store from '@/store/index.js'
+
+describe('Search rooms by city', () => {
+ let requestPayload = {"city_id":115,"start_date":"2020-05-01T11:00:23.738Z","end_date":"2020-05-03T11:00:23.739Z","nr_of_rooms":1}
+ let expectedRequestResponse = 
+
+    test('', () => {
+        fetch.mockResponseOnce();
+        await store.dispatch();
+        expect(store.state.).to.eql(value);
+    });
+
+    
+});
