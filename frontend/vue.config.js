2e31a57b07d0462c722abf7ea1298707e3fb2881
Janis <janis_k@hotmail.com>
2020-05-03 10:15:55 +0200

    Sorting + clean-up
    Fixed the sorting of hotels and a wee bit of code clean-up

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,21 +1,26 @@
 module.exports = {
   devServer: {
     proxy: {
-      '^/rest': {
-        target: 'http://localhost:8070',
+      "^/rest": {
+        target: "http://localhost:8070",
         ws: true,
         changeOrigin: true,
       },
-      '^/login': {
-        target: 'http://localhost:8070',
+      "^/login": {
+        target: "http://localhost:8070",
         ws: true,
         changeOrigin: true,
       },
-      '^/api': {
-        target: 'http://localhost:8070',
+      "^/logout": {
+        target: "http://localhost:8070",
+        ws: true,
+        changeOrigin: true,
+      },
+      "^/api": {
+        target: "http://localhost:8070",
         ws: true,
         changeOrigin: true,
       },
     },
   },
-}
+};
