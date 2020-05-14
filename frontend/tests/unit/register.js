4712191ff39721036c4242f417fafb578d1ccbdd
Steve Chapman <srchap78@gmail.com>
2020-05-14 12:29:53 +0200

    userStatus.spec for simple testing of mutations

--- frontend/tests/unit/register.js
+++ frontend/tests/unit/register.js
@@ -1,3 +1,4 @@
 import { mount } from '@vue/test-utils'
 import Register from '@/views/Register.vue'
 
+
---
+++ frontend/tests/unit/userStatus.spec.js
@@ -0,0 +1,30 @@
+//just using this at the moment to load code -- work on this later to better understanding
+import { enableFetchMocks } from 'jest-fetch-mock'
+enableFetchMocks()
+import store from '@/store/index.js'
+
+//imports assert,expect and should from chai's assertion library so we can use https://www.chaijs.com/api/bdd/
+import {assert} from 'chai'
+import {expect} from 'chai'
+import {should} from 'chai'
+
+//call a mutation with .commit('name', paramater)
+//call an action with .dispatch('name', paramater)
+
+describe('Change value of userLogged', () => {
+    test('should start with: value = false', async () => {
+
+        console.log("userLogged starts as: "+store.state.userLogged);
+        assert.isFalse(store.state.userLogged, "should be false");
+    });
+
+    test('After commit: value = true', () => {
+        console.log("userLogged before commit: "+store.state.userLogged);
+        store.commit('userLogStatus', true)
+        console.log("userLogged after commit: "+store.state.userLogged);
+
+        assert.isTrue(store.state.userLogged, "Should expect to be true");
+        expect(store.state.userLogged).to.be.a("boolean");
+        
+    });    
+});
